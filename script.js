58fa1492160400cef1eab9b66ea630d938ca93ef
Thais Luza <thaisluza21@hotmail.com>
2021-10-27 17:56:49 -0300

    finalizando requisitos obrigatorios

--- script.js
+++ script.js
@@ -1,64 +1,41 @@
-let tarefa = ''
-
-let listaOrdenada = document.getElementById('lista-tarefas');
+const listaOrdenada = document.getElementById('lista-tarefas');
 const botaoAdiciona = document.getElementById('criar-tarefa');
-let listaLi = document.getElementsByTagName('li')
-let selected = document.getElementsByClassName('selected')
+const listaLi = document.getElementsByTagName('li');
+const input = document.getElementById('texto-tarefa');
+let selected = document.getElementsByClassName('selected');
+const botaoLimpar = document.getElementById('apaga-tudo');
+const botaoRemover = document.getElementById('remover-finalizados');
+let completed = document.getElementsByClassName('completed');
 
-botaoAdiciona.addEventListener('click', adicionaTarefa)
+botaoAdiciona.addEventListener('click', function () {
+  let li = document.createElement('li');
+  li.innerText = input.value;
+  listaOrdenada.appendChild(li);
+  input.value = '';
+});
 
-function adicionaTarefa(){
- tarefa = document.getElementById('texto-tarefa').value
- const novaTarefa = document.createElement('li');
- novaTarefa.innerText = tarefa
- listaOrdenada.appendChild(novaTarefa);
- document.getElementById('texto-tarefa').value = ''
- cor()
+listaOrdenada.addEventListener('click', function (event) {
+  if (selected[0]) {
+    selected[0].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+});
 
-}
+listaOrdenada.addEventListener('dblclick', function (event) {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+});
 
+botaoLimpar.addEventListener('click', function () {
+  listaOrdenada.innerText = '';
+});
 
-function cor(){
-for(let i = 0; i <listaLi.length; i++){
-    listaLi[i].addEventListener('click', function(){
-      selected[0].classList.remove('selected')
-      listaLi[i].className = 'selected'
-   
-  })
-}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  localStorage.setItem(tarefa,tarefa)
-
-//  criaTarefa(tarefa)
-// }
-
-// window.onload = function (){
-//   if(localStorage.length>0){
-//     for(let i =0; i<localStorage.length; i++){
-//       let key = localStorage.key(i)
-//       let objeto = localStorage[key]
-
-//     }
-//   }
-//}
-
-
-
+botaoRemover.addEventListener('click', function () {
+  let completed = document.getElementsByClassName('completed');
+  while (completed.length > 0) {
+    completed[0].parentNode.removeChild(completed[0]);
+  }
+});
